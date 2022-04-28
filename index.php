e9e4bf706fa066f127852431af62190c69f7bbd2
Legrand Thomas <pro.legrand.thomas@gmail.com>
2022-04-28 18:37:08 +0200

    prise de tête ^^

--- index.php
+++ index.php
@@ -1,40 +1,90 @@
+<?php 
+session_start();
+if (!key_exists("Prenom", $_SESSION) ||
+    !key_exists("Nom", $_SESSION) ||
+    !key_exists("Age", $_SESSION)||
+    !key_exists("Taille", $_SESSION)||
+    !key_exists("civilité", $_SESSION)) {
+    $has_session = session_status() == PHP_SESSION_ACTIVE;
+    
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
-    <?php  include 'includes/head.inc.html';  ?>
+    <?php include("./includes/head.inc.html"); ?> 
+
 <body>
-     <?php include 'includes/header.inc.html';  ?>
-     
 
-     <div class="container">
+    <?php include("./includes/header.inc.html"); ?> 
+
+    <div class="container">
         <div class="row">
-            <div class="col-3">
-                <nav>
-                    <div class="d-grid gap-2">
-                    
-                            <button type="button" class="btn btn-outline-secondary btn-block">Home</button>
-                    
-                    </div>
-                </nav>
-            </div>
+            <div class="col-md-3 mt-3">
+            
+            <a role="button" class="btn btn-outline-secondary w-100" href="index.php" name="home">Home</a> 
+           <?php  if ($has_session = 1) include_once './includes/ul.inc.php'; 
+           ?>
+          
+             </div>
 
-            <div class="col-9">         
-                <section>
-                    <button type="button" class="btn btn-primary">Ajouter des données</button>
+            <section class="col-md-9 mt-3">
+
+                   
+                   
+                    <?php
+
+                     if(isset($_GET['add'])) {include './includes/form.inc.html'; }
+                     else{
+                        
+                   
+                                      if(isset($_POST['enreg'])){
+                
+                                              $table = array(
+                                              $first_name = $_POST['Prenom'],
+                                              $last_name = $_POST['Nom'],
+                                              $age= $_POST['Age'],
+                                              $size = $_POST['Taille'],
+                                              $civility= $_POST['civilité'],
+                                                            );             
+           
+                                              $_SESSION['table'] = $table; 
+                                            // print_r ($_SESSION['table']);  
+              
+                                             echo '<p class="alert-success text-center py-3"> Données sauvegardées</p>' ;
+                                            
+                                            }  else{
+                                               
+                                            echo '<a role="button" class=" btn btn-primary" href="index.php?add">Ajouter des données</a>';
+                                                
+                                            }    
+                                             
+                                                              
+                                        } 
+                                        
+                             
+                                           
+                            
+                          
+                                                                              
+                    ?>
+                  
+
+
+
                     
-                    <?php  include 'includes/form.inc.html'; ?>
-                </section>
-            </div>
-        </div>   
+
+                  
+<!--
+
+-->
+            </section>
+        </div>
+
     </div>
-    
-       
-         
-         <!-- include 'includes/ul.inc.php'; -->
-         
-         
-         
-    <?php include 'includes/footer.inc.html';  ?>
-
+    <br>
+    <?php include("./includes/footer.inc.html"); ?> 
 </body>
 </html>
