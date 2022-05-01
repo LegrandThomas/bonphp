f5e060900020c49c15e034d2748df8741580c76c
Legrand Thomas <pro.legrand.thomas@gmail.com>
2022-05-01 12:06:55 +0200

    PAS LOIN

--- index.php
+++ index.php
@@ -17,51 +17,45 @@ if (isset($_SESSION['table'])) $table=$_SESSION['table'];
 <!DOCTYPE html>
 <html lang="fr">
 
-    <?php include("./includes/head.inc.html"); ?> 
+<?php include("./includes/head.inc.html"); ?> 
 
 <body>
 
-    <?php include("./includes/header.inc.html"); ?> 
+<?php include("./includes/header.inc.html"); ?> 
 
-    <div class="container">
-        <div class="row">
-        <nav  class="col-md-3 mt-3">
-                <a role="button" class="btn btn-outline-secondary w-100" href="index.php">Home</a> 
-                <?php 
-                if (isset($table)) include_once './includes/ul.inc.php';
-                 ?>  
-               
-        </nav>
+<div class="container">
+<div class="row">
+<nav  class="col-md-3 mt-3">
+    <a role="button" class="btn btn-outline-secondary w-100" href="index.php">Home</a> 
+    <?php 
+     if (isset($table)) include_once './includes/ul.inc.php';
+    ?>  
+</nav>
            
-            <section class="col-md-9 mt-3">
+<section class="col-md-9 mt-3">
 
-                    <?php
+  <?php
                   
-                     if(isset($_GET['add'])) {include './includes/form.inc.html'; }
+    if(isset($_GET['add'])) {include './includes/form.inc.html'; }
 
-                             elseif (isset($_POST['enreg'])){
+        elseif (isset($_POST['enreg'])){
                                           
-                                               $table = array( 
-                                              "first_name" => $_POST['Prenom'],
-                                              "last_name" => $_POST['Nom'],
-                                              "age"=> $_POST['Age'],
-                                              "size" => $_POST['Taille'],
-                                              "civility"=> $_POST['inlineRadioOptions']
-                                              
-                                                            );             
+             $table = array( 
+            "first_name" => $_POST['Prenom'],
+            "last_name" => $_POST['Nom'],
+            "age"=> $_POST['Age'],
+            "size" => $_POST['Taille'],
+            "civility"=> $_POST['inlineRadioOptions']
+                            );             
            
-                                            $_SESSION['table'] = $table; 
-                                    
-                                        
-                                             echo '<p class="alert-success text-center py-3"> Données sauvegardées</p>' ;
+            $_SESSION['table'] = $table; 
+                           
+            echo '<p class="alert-success text-center py-3"> Données sauvegardées</p>' ;
                                            
-                                            }     else{
+                                        } else{
                                                 if (isset($table)){
-                                                
-                                                
-                                                         
-                                                switch(isset($_GET))
-                                                {
+                                                       
+                                                    switch(isset($_GET)){
                                                     case isset($_GET['debugging']):
                                                        //echo ' passé en debugging';
                                                        echo '<h2 class="text-center">Débogage</h2>';
@@ -143,19 +137,17 @@ if (isset($_SESSION['table'])) $table=$_SESSION['table'];
                                                     break;
         
                                                
-                                                }
-                                                   }
+                                                                        }
+                                                                    }
                                                    echo '<a role="button" class=" btn btn-primary" href="index.php?add">Ajouter des données</a>';
                                         
-                                        
-                                      
-                                    } 
+                                               } 
                                                    
                                
-                    ?>
+  ?>
                   
 
-      </section>
+</section>
         </div>
 
     </div>
